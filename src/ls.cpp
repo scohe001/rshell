965de5fcac1d646456b34e18ef9c4af589bac7c1
Ace Rhino <a12n@mac.com>
2015-02-08 14:32:14 -0800

    Added color macros

--- src/ls.cpp
+++ src/ls.cpp
@@ -2,18 +2,37 @@
 #include <dirent.h>
 #include <errno.h>
 #include <pwd.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 #include <iostream>
 
 using namespace std;
 
-/*
- * This is a BARE BONES example of how to use opendir/readdir/closedir.
- * Notice that there is no error checking on these functions.
- * You MUST add error checking yourself.
- */
+//the following are UBUNTU/LINUX ONLY terminal color codes.
+#define RESET   "\033[0m"
+#define BLACK   "\033[30m"      /* Black */
+#define RED     "\033[31m"      /* Red */
+#define GREEN   "\033[32m"      /* Green */
+#define YELLOW  "\033[33m"      /* Yellow */
+#define BLUE    "\033[34m"      /* Blue */
+#define MAGENTA "\033[35m"      /* Magenta */
+#define CYAN    "\033[36m"      /* Cyan */
+#define WHITE   "\033[37m"      /* White */
+#define BOLDBLACK   "\033[1m\033[30m"      /* Bold Black */
+#define BOLDRED     "\033[1m\033[31m"      /* Bold Red */
+#define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
+#define BOLDYELLOW  "\033[1m\033[33m"      /* Bold Yellow */
+#define BOLDBLUE    "\033[1m\033[34m"      /* Bold Blue */
+#define BOLDMAGENTA "\033[1m\033[35m"      /* Bold Magenta */
+#define BOLDCYAN    "\033[1m\033[36m"      /* Bold Cyan */
+#define BOLDWHITE   "\033[1m\033[37m"      /* Bold White */
 
 int main(int argc, char *argv[]) {
+    
+    //struct stat s;
+    //stat("test",&s);
+    //std::cout << "num bytes: " << s.st_size << std::endl;
     
     char dirName[] = ".";
     DIR *dirp = opendir(dirName);
