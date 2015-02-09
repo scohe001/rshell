11e574c4f38db1eece4b244cddf12fc5f3b0c7d5
Ace Rhino <a12n@mac.com>
2015-02-09 02:01:09 -0800

    Added ls tests

---
+++ tests/tests_ls.sh
@@ -0,0 +1,71 @@
+bin/ls
+echo
+bin/ls src #test a dir
+echo
+bin/ls src bin #multiple dirs
+echo
+bin/ls Makefile #test a file
+echo 
+bin/ls MakeFile README.md #multiple files
+echo 
+bin/ls src Makefile #mix dir and file
+echo 
+bin/ls -a #test a flag
+echo 
+bin/ls -a src #flag with dir
+echo 
+bin/ls -a Makefile #flag with file
+echo 
+bin/ls -a src bin #flag with multiple dirs
+echo 
+bin/ls -l . #l flag
+echo 
+bin/ls -R . #R flag
+echo 
+bin/ls -la #multiple flags
+echo 
+bin/ls -lllllllllaaaaaaaaaaaa
+echo 
+bin/ls -a -a -a -a -a -a -a
+echo 
+bin/ls -a src -l #bad flag
+echo 
+bin/ls -z #bad flag
+echo 
+bin/ls -aaaaaaaaaalllllllllz #bad flag
+echo 
+bin/ls asdfasdfasdf #bad file
+echo 
+bin/ls -a asdfasdfasdf #bad file with flags
+echo 
+bin/ls src asdfasdfasdf bin #bad file with other files
+echo 
+#bin/ls
+#bin/ls
+#bin/ls
+#bin/ls
+#bin/ls
+#bin/ls
+#bin/ls
+#bin/ls
+#bin/ls
+#bin/ls
+#bin/ls
+#bin/ls
+#bin/ls
+#bin/ls
+#bin/ls
+#bin/ls
+#bin/ls
+#bin/ls
+#bin/ls
+#bin/ls
+#bin/ls
+#bin/ls
+#bin/ls
+#bin/ls
+#bin/ls
+#bin/ls
+#bin/ls
+#bin/ls
+#bin/ls
